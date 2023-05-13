5043afc324fa8983747cce39bc40478a43d603b2
samuelowino <samuelowino43@yahoo.com>
2023-05-13 12:28:12 +0300

    Data driven navigation links

---
+++ ios/iOS16Navigation/iOS16NavigationLinks/iOS16NavigationLinks/ContentView.swift
@@ -0,0 +1,98 @@
+//
+//  ContentView.swift
+//  iOS16NavigationLinks
+//
+//  Created by Samuel Owino on 13/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
+    
+    var body: some View {
+        NavigationStack(path: $viewModel.path) {
+            List(Category.allCases, id: \.self) { category in
+                Section(category.rawValue) {
+                    ForEach(recipes(in: category)) { recipe in
+                        NavigationLink(recipe.name, value: recipe)
+                            .onTapGesture {
+                                viewModel.openRecipe(recipe)
+                            }
+                    }
+                }
+            }
+        }
+        .navigationTitle("Old NavigationLinks")
+        .navigationDestination(for: Recipe.self) { recipe in
+            // The destination view builder
+            Label("Recipet: \(recipe.name)", systemImage: "wallet")
+        }
+    }
+}
+
+class ViewModel: ObservableObject {
+    // The navigation path
+    @Published var path:[Recipe] = []
+    
+    func openRecipe(_ recipe: Recipe) {
+        path.append(recipe)
+    }
+
+    func popToRoot(){
+        path.removeAll()
+    }
+}
+
+enum Category: String, CaseIterable {
+    case milk = "Milk"
+    case drinks = "Drinks"
+    case meat = "Meat"
+    case other = "Other"
+}
+
+struct Recipe: Identifiable, Hashable {
+    var id: UUID = UUID()
+    var name: String = ""
+    var category: Category = .other
+}
+
+private func recipes(in category: Category) -> [Recipe] {
+    switch category {
+    case .milk:
+        return [
+            Recipe(name: "Falifa stew", category: category),
+            Recipe(name: "Maliwa", category: category),
+            Recipe(name: "Dafi", category: category),
+            Recipe(name: "Lifi", category: category),
+        ]
+    case .drinks:
+        return [
+            Recipe(name: "Gangi", category: category),
+            Recipe(name: "Nagi", category: category),
+            Recipe(name: "Zingi", category: category),
+            Recipe(name: "Bambi", category: category),
+        ]
+    case .meat:
+        return [
+            Recipe(name: "Stewa", category: category),
+            Recipe(name: "Ronada", category: category),
+            Recipe(name: "Wuqata", category: category),
+            Recipe(name: "Muspata", category: category),
+        ]
+    case .other:
+        return [
+            Recipe(name: "Jingat stew", category: category),
+            Recipe(name: "Ria", category: category),
+            Recipe(name: "Noif", category: category),
+            Recipe(name: "Waquia", category: category),
+        ]
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/iOS16Navigation/iOS16NavigationLinks/iOS16NavigationLinks/iOS16NavigationLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS16NavigationLinksApp.swift
+//  iOS16NavigationLinks
+//
+//  Created by Samuel Owino on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS16NavigationLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
