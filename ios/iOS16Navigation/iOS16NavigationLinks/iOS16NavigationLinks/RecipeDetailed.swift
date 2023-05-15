edc9c8aaeeaff0bda7ea9881a5e22edc8ff502e7
samuelowino <samuelowino43@yahoo.com>
2023-05-15 15:23:23 +0300

    Navigation stack, destination correction

---
+++ ios/iOS16Navigation/iOS16NavigationLinks/iOS16NavigationLinks/RecipeDetailed.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeDetailed.swift
+//  iOS16NavigationLinks
+//
+//  Created by Samuel Owino on 15/05/2023.
+//
+
+import SwiftUI
+
+struct RecipeDetailed: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeDetailed_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeDetailed()
+    }
+}
